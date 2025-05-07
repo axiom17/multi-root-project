7475740d3ac5edaea06e7677ae662765de166c49
E. Levi Allen <e.levi.allen@live.com>
2025-05-07 17:42:06 -0400

    initial commit

---
+++ included-project-a/src/test/java/example/A.java
@@ -0,0 +1,11 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class A {
+    @Test
+    void testA() {
+        assertTrue(true);
+    }
+}
---
+++ included-project-b/src/test/java/example/B.java
@@ -0,0 +1,11 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class B {
+    @Test
+    void testB() {
+        assertEquals(1, 1);
+    }
+}
